c01d1f347714f8c6e4af3899408b5fec33685093
SparrowSurya <thakur.suryakant.0406@gmail.com>
2022-11-12 22:45:00 +0530

    generate implementation

--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -17,21 +17,24 @@ typedef unsigned short udtype_t;
 typedef int sdtype_t;
 
 /* cell states */
-typedef enum {
+typedef enum
+{
     HIDDEN,
     OPENED,
     FLAGGED
 } state_t;
 
 /* cell structure */
-typedef struct {
+typedef struct
+{
     state_t state = HIDDEN;
     bool is_mine = false;
     udtype_t mines = 0;
 } cell_t;
 
 /* coord as r, c */
-typedef struct {
+typedef struct
+{
     sdtype_t r = None;
     sdtype_t c = None;
 } coord_t;
@@ -39,21 +42,22 @@ typedef struct {
 /* global variables */
 
 cell_t **Mineboard = nullptr;
-udtype_t Rows  = 0;
-udtype_t Cols  = 0;
+udtype_t Rows = 0;
+udtype_t Cols = 0;
 udtype_t Mines = 0;
-
 
 /* functions declaration */
 
-void generate();
-bool resize(udtype_t, udtype_t);
-void print();
-udtype_t random(udtype_t);
-coord_t *neighbour8(udtype_t, udtype_t);
-void print_sep();
-void print_cnt(udtype_t);
-
+void generate();                            /* generates mineboard mines */
+void reset();                               /* erases all data to default in mineboard */
+void increment_mines(udtype_t, udtype_t);   /* increments mines count in neighbours */
+bool resize(udtype_t, udtype_t);            /* resizes the mineboard */
+void first_guess_wrong(udtype_t, udtype_t); /* prevents the first selected cell to be mine */
+udtype_t random(udtype_t);                  /* returns random number including end */
+coord_t *neighbour8(udtype_t, udtype_t);    /* returns the neighbour cell coordinates */
+void print();                               /* outputs mineboard to console */
+void print_sep();                           /* sub-function of print function to print single seperating row */
+void print_cnt(udtype_t);                   /* sub-function of print to print single row elemnts of mineboard based on their data */
 
 int main()
 {
@@ -62,78 +66,123 @@ int main()
     return 0;
 }
 
+/* to generate mineboard */
+void generate()
+{
+    udtype_t m = Mines, r, c;
+
+    while (m)
+    {
+        r = random(Rows - 1);
+        c = random(Cols - 1);
+
+        if (Mineboard[r][c].is_mine == false)
+        {
+            Mineboard[r][c].is_mine = true;
+            increment_mines(r, c);
+            m--;
+        }
+    }
+}
+/* resizes the mineboard */
 bool resize(udtype_t rows, udtype_t cols)
 {
-    if (MINROW>rows && rows>MAXROW && MINCOL>cols && cols>MAXCOL) {
+    if (MINROW > rows && rows > MAXROW && MINCOL > cols && cols > MAXCOL)
+    {
         return false;
     }
-    if (Mineboard != nullptr) {
+    if (Mineboard != nullptr)
+    {
         delete[] Mineboard;
         Mineboard = nullptr;
     }
     Rows = rows;
     Cols = cols;
-    Mineboard = new cell_t* [Rows];
-    for (udtype_t i = 0; i < Cols; i++) {
+    Mineboard = new cell_t *[Rows];
+    for (udtype_t i = 0; i < Cols; i++)
+    {
         Mineboard[i] = new cell_t[Cols];
     }
     return true;
 }
 
-void print() {
+/* outputs mineboard to console */
+void print()
+{
     std::cout << '\n';
     print_sep();
-    for (udtype_t i = 0; i < Rows; i++) {
+    for (udtype_t i = 0; i < Rows; i++)
+    {
         print_cnt(i);
         print_sep();
     }
     std::cout << '\n';
 }
 
-/* 0 to end including end */
-udtype_t random(udtype_t end) {
-    return rand() % (end+1);
+/* returns random number including end */
+udtype_t random(udtype_t end)
+{
+    return rand() % (end + 1);
 }
 
-coord_t *neighbour8(udtype_t r, udtype_t c) {
+/* returns the neighbour cell coordinates */
+coord_t *neighbour8(udtype_t r, udtype_t c)
+{
     coord_t *neighbour = new coord_t[8];
     udtype_t i = 0;
     sdtype_t rel_dir[8][2] = {
-        {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}
-    };
+        {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
 
-    for (auto d: rel_dir) {
-        if (MINROW <= r+d[0] &&r+d[0]<=MAXROW && MINCOL <= c+d[1] &&c+d[1]<=MAXCOL) {
-            neighbour[i].r = r+d[0];
-            neighbour[i].c = c+d[1];
+    for (auto d : rel_dir)
+    {
+        if (MINROW <= r + d[0] && r + d[0] <= MAXROW && MINCOL <= c + d[1] && c + d[1] <= MAXCOL)
+        {
+            neighbour[i].r = r + d[0];
+            neighbour[i].c = c + d[1];
         }
     }
     return neighbour;
 }
 
-void print_cnt(udtype_t r) {
+/* sub-function of print to print single row elemnts of mineboard based on their data */
+void print_cnt(udtype_t r)
+{
     std::cout << '|';
-    for (udtype_t c=0; c<Cols; c++) {
-        if (Mineboard[r][c].state == HIDDEN) {
-            std::cout << (char)176 << (char)176 << (char)176 << '|'; 
-        } else if (Mineboard[r][c].state == OPENED) {
-            if (Mineboard[r][c].mines == 0) {
+    for (udtype_t c = 0; c < Cols; c++)
+    {
+        if (Mineboard[r][c].state == HIDDEN)
+        {
+            std::cout << (char)176 << (char)176 << (char)176 << '|';
+        }
+        else if (Mineboard[r][c].state == OPENED)
+        {
+            if (Mineboard[r][c].mines == 0)
+            {
                 std::cout << "   ";
-            } else {
+            }
+            else
+            {
                 std::cout << ' ' << Mineboard[r][c].mines << ' ' << '|';
             }
-        } else if (Mineboard[r][c].state == FLAGGED) {
+        }
+        else if (Mineboard[r][c].state == FLAGGED)
+        {
             std::cout << ' ' << (char)254 << ' ' << '|';
-        } else {
+        }
+        else
+        {
             throw "Unexpected cause for board cell has undefined state";
         }
     }
     std::cout << '\n';
 }
 
-void print_sep() {
+/* sub-function of print function to print single seperating row */
+void print_sep()
+{
     std::cout << '+';
-    for (udtype_t i=0; i<Cols; i++) {
+    for (udtype_t i = 0; i < Cols; i++)
+    {
         std::cout << "---+";
     }
     std::cout << '\n';
