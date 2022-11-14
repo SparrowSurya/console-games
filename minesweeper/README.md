4ac5b2d13f2199e137d565cbb308579aff6fda5c
SparrowSurya <thakur.suryakant.0406@gmail.com>
2022-11-14 13:09:06 +0530

    removed all declaration funcs, re arranged functions order, main func at end ith some sample testing code

--- minesweeper/README.md
+++ minesweeper/README.md
@@ -2,3 +2,6 @@
 
 ---
 
+#### Pending:
+1. to print flagged tile
+2. to input the type of command
--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -36,186 +36,28 @@ num_t Rows = 0;
 num_t Cols = 0;
 num_t Mines = 0;
 
-/* functions declaration */
-
-void generate();                           /* generates mineboard mines */
-void reset();                              /* erases all data to default in mineboard */
-void modify_adjacent(num_t, num_t, short); /* changes mines amount in adjacent tiles */
-bool resize(num_t, num_t, num_t);          /* resizes the mineboard */
-void first_guess_wrong(num_t, num_t);      /* prevents the first selected tile to be mine */
-num_t random(num_t);                       /* returns random number including end */
-void print();                              /* outputs mineboard to console */
-void print_sep();                          /* sub-function of print function to print single seperating row */
-void print_cnt(num_t);                     /* sub-function of print to print single row elemnts of mineboard based on their data */
-bool press(num_t, num_t);                  /* to unhide a tile */
-
-int main() // MAIN
-{
-    srand(time(0)); // random seed for each program instance
-
-    // test
-
-    resize(5, 5, 8);
-    generate();
-    print();
-
-    for (num_t r = 0; r < Rows; r++)
-    {
-        for (num_t c = 0; c < Cols; c++)
-        {
-            press(r, c);
-        }
-    }
-    print();
-
-    std::cout << "\nSUCESSFUL EXIT\n";
-    return 0;
-}
-
-/* to generate mineboard */
-void generate()
-{
-    num_t m = Mines, r, c;
-
-    while (m > 0) // placing mines
-    {
-        r = random(Rows - 1);
-        c = random(Cols - 1);
-
-        if (Mineboard[r][c].is_mine == false)
-        {
-            Mineboard[r][c].is_mine = true;
-            modify_adjacent(r, c, 1);
-            m--;
-        }
-    }
-}
-
-/* to unhide a tile */
-bool press(num_t r, num_t c)
-{
-    if (0 <= r && r < Rows && 0 <= c && c < Cols)
-    {
-        Mineboard[r][c].state = OPENED;
-        return true;
-    }
-    return false;
-}
-
-/* resizes the mineboard */
-bool resize(num_t rows, num_t cols, num_t mines)
-{
-    if (MINROW > rows && rows > MAXROW && MINCOL > cols && cols > MAXCOL)
-    {
-        return false;
-    }
-    if (Mineboard != nullptr)
-    {
-        delete[] Mineboard;
-        Mineboard = nullptr;
-    }
-    Rows = rows;
-    Cols = cols;
-    Mines = mines;
-    Mineboard = new tile_t *[Rows];
-    for (num_t i = 0; i < Cols; i++)
-    {
-        Mineboard[i] = new tile_t[Cols];
-    }
-    return true;
-}
-
-/* prevents the first selected tile to be mine */
-void first_guess_wrong(num_t r, num_t c)
-{
-    modify_adjacent(r, c, -1);
-    do
-    {
-        r = random(Rows - 1);
-        c = random(Cols - 1);
-    } while (Mineboard[r][c].is_mine == true);
-    modify_adjacent(r, c, +1);
-}
-
-/* erases all data to default in mineboard */
-void reset()
-{
-    for (num_t r = 0; r < Rows; r++)
-    {
-        for (num_t c = 0; c < Cols; c++)
-        {
-            Mineboard[r][c].is_mine = false;
-            Mineboard[r][c].mines = 0;
-            Mineboard[r][c].state = CLOSED;
-        }
-    }
-}
-
-/* changes mines amount in adjacent tiles */
-void modify_adjacent(num_t r, num_t c, short amount)
-{
-    short rel_dir[8][2] = {
-        {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
-
-    for (auto &d : rel_dir)
-    {
-        if (0 <= (r + d[0]) && (r + d[0]) < Rows && 0 <= (c + d[1]) && (c + d[1]) < Cols)
-        {
-            Mineboard[r + d[0]][c + d[1]].mines += amount;
-        }
-    }
-}
-
-/* outputs mineboard to console */
-void print()
-{
-    std::cout << '\n';
-    print_sep();
-    for (num_t i = 0; i < Rows; i++)
-    {
-        print_cnt(i);
-        print_sep();
-    }
-    std::cout << '\n';
-}
-
-/* returns random number including end */
-num_t random(num_t end)
-{
-    return rand() % (end + 1);
-}
 
 /* sub-function of print to print single row elemnts of mineboard based on their data */
-void print_cnt(num_t r)
-{
+void print_cnt(num_t r) {
     std::cout << '|';
-    for (num_t c = 0; c < Cols; c++)
-    {
-        if (Mineboard[r][c].state == CLOSED)
-        {
+    for (num_t c = 0; c < Cols; c++) {
+
+        if (Mineboard[r][c].state == CLOSED) {
             std::cout << (char)176 << (char)176 << (char)176 << '|';
-        }
-        else if (Mineboard[r][c].state == OPENED)
-        {
-            if (Mineboard[r][c].is_mine)
-            {
+
+        } else if (Mineboard[r][c].state == OPENED) {
+
+            if (Mineboard[r][c].is_mine) {
                 std::cout << ' ' << (char)254 << ' ' << '|';
-            }
-            else if (Mineboard[r][c].mines == 0)
-            {
+            } else if (Mineboard[r][c].mines == 0) {
                 std::cout << "   |";
-            }
-            else
-            {
+            } else {
                 std::cout << ' ' << Mineboard[r][c].mines << ' ' << '|';
             }
         }
-        else if (Mineboard[r][c].state == FLAGGED)
-        {
+        else if (Mineboard[r][c].state == FLAGGED) {
             std::cout << ' ' << (char)254 << ' ' << '|';
-        }
-        else
-        {
+        } else {
             throw "Unexpected cause for board tile has undefined state";
         }
     }
@@ -223,12 +65,133 @@ void print_cnt(num_t r)
 }
 
 /* sub-function of print function to print single seperating row */
-void print_sep()
-{
+void print_sep() {
     std::cout << '+';
-    for (num_t i = 0; i < Cols; i++)
-    {
+    for (num_t i = 0; i < Cols; i++) {
         std::cout << "---+";
     }
     std::cout << '\n';
+}
+
+/* outputs mineboard to console */
+void print() {
+    std::cout << '\n';
+    print_sep();
+    for (num_t i = 0; i < Rows; i++) {
+        print_cnt(i);
+        print_sep();
+    }
+    std::cout << '\n';
+}
+
+/* returns random number excluding end */
+num_t random(num_t end) {
+    return rand() % end;
+}
+
+/* erases all data to default in mineboard */
+void reset() {
+    for (num_t r = 0; r < Rows; r++) {
+        for (num_t c = 0; c < Cols; c++) {
+            Mineboard[r][c].is_mine = false;
+            Mineboard[r][c].mines = 0;
+            Mineboard[r][c].state = CLOSED;
+        }
+    }
+}
+
+/* resizes the mineboard */
+bool resize(num_t rows, num_t cols, num_t mines) {
+    if (MINROW > rows && rows > MAXROW && MINCOL > cols && cols > MAXCOL) {
+        return false;
+    }
+    if (Mineboard != nullptr) {
+        delete[] Mineboard;
+        Mineboard = nullptr;
+    }
+
+    Rows = rows;
+    Cols = cols;
+    Mines = mines;
+    Mineboard = new tile_t *[Rows];
+
+    for (num_t i = 0; i < Cols; i++) {
+        Mineboard[i] = new tile_t[Cols];
+    }
+    return true;
+}
+
+/* changes mines amount in adjacent tiles */
+void modify_adjacent(num_t r, num_t c, short amount) {
+    short rel_dir[8][2] = {
+        {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}
+    };
+
+    for (auto &d : rel_dir) {
+        if (0 <= (r + d[0]) && (r + d[0]) < Rows && 0 <= (c + d[1]) && (c + d[1]) < Cols) {
+            Mineboard[r + d[0]][c + d[1]].mines += amount;
+        }
+    }
+}
+
+/* to generate mineboard */
+void generate() {
+    num_t m = Mines, r, c;
+
+    while (m > 0) {
+        r = random(Rows - 1);
+        c = random(Cols - 1);
+
+        if (Mineboard[r][c].is_mine == false) {
+            Mineboard[r][c].is_mine = true;
+            modify_adjacent(r, c, 1);
+            m--;
+        }
+    }
+}
+
+/* prevents the first selected tile to be mine */
+void first_guess_wrong(num_t r, num_t c) {
+    modify_adjacent(r, c, -1);
+    do {
+        r = random(Rows);
+        c = random(Cols);
+    } while (Mineboard[r][c].is_mine == true);
+    modify_adjacent(r, c, +1);
+}
+
+/* to unhide a tile */
+bool press(num_t r, num_t c) {
+    if (0 <= r && r < Rows && 0 <= c && c < Cols && Mineboard[r][c].state == CLOSED) {
+        Mineboard[r][c].state = OPENED;
+        return true;
+    }
+    return false;
+}
+
+int main() {
+
+    // initializations
+    srand(time(0)); // random seed for each program instance
+    num_t row_in, col_in, mines_in; // row & col & mine count inputs
+    short mines_left; // mines left in board; flagged tiles are counted as correct
+
+    resize(5, 5, 8);
+    generate();
+
+    // game starts
+    std::cout << '\n' << "--------------------Minesweeper--------------------" << "\n\n";
+
+    // testing
+
+    while (1) {
+
+        print();
+        std::cout << "Enter row col to break Tile: \n" << ">>> ";
+        std::cin >> row_in >> col_in;
+        press(row_in, col_in);
+    }
+
+    std::cout << "\nSUCESSFUL EXIT\n";
+    return 0;
 }
